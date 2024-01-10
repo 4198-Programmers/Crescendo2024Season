bcb51a2115adc02d9d877675bc96275ce8d7f55e
EmilyKram <90582499+EmilyKram@users.noreply.github.com>
2024-01-09 23:22:35 -0600

    Starting on Swerve Modules

--- src/main/java/frc/robot/subsystems/Swerve/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/Swerve/SwerveModule.java
@@ -1,7 +1,10 @@
 package frc.robot.subsystems.Swerve;
 
+import com.ctre.phoenix.sensors.WPI_CANCoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+
+import edu.wpi.first.math.controller.PIDController;
 
 /**This is the class to set up each swerve module.
  * <p>
@@ -14,5 +17,16 @@ public class SwerveModule {
     private CANSparkMax angleMotor;
     /**Used to get information from the motor */
     private RelativeEncoder driveEncoder;
-    
+    /**Used to let angle motors remember their positions */
+    private WPI_CANCoder angleEncoder;
+    /**This is used to have the angle motors go to the right spot */
+    private PIDController anglePIDController;
+    /**Used in println's to make it easier to see */
+    private String swerveModuleName;
+    /**Used in arrays to make it easier to read */
+    private int moduleNumber;
+
+    public SwerveModule(String swerveModuleName, int moduleNumber; int driveMotorID, int angleMotorID, int angleEncoderID, double angleOffsetDegrees){
+        
+    }
 }
