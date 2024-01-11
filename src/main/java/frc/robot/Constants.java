77d3ce7078a8c03316d308c09a0c1d4a9f2912e0
EmilyKram <90582499+EmilyKram@users.noreply.github.com>
2024-01-11 17:40:45 -0600

    All swerve is done, just need PID Controller working

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 
 /**
@@ -17,7 +18,7 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 public final class Constants {
   //Angle Constants
   public static final boolean ANGLE_MOTOR_INVERTED = false;
-  public static final double VOLTAGE_COMPENSATION = 0;
+  public static final double VOLTAGE_COMPENSATION = 12.0; //Max battery volts we can half
   public static final boolean ANGLE_ENCODER_DIRECTION = false;
   //Angle PID Controller Constants
   public static final double ANGLE_KP = 0;
@@ -47,7 +48,42 @@ public final class Constants {
   public static final int BACK_LEFT_ANGLE_MOTOR_ID = 0;
   public static final int BACK_LEFT_ANGLE_ENCODER_ID = 0;
   public static final double BACK_LEFT_ANGLE_OFFSET_DEGREES = 0;
+  //Back Right Module Constants
+  public static final String BACK_RIGHT_MODULE_NAME = null;
+  public static final int BACK_RIGHT_MODULE_NUMBER = 0;
+  public static final int BACK_RIGHT_DRIVE_MOTOR_ID = 0;
+  public static final int BACK_RIGHT_ANGLE_MOTOR_ID = 0;
+  public static final int BACK_RIGHT_ANGLE_ENCODER_ID = 0;
+  public static final double BACK_RIGHT_ANGLE_OFFSET_DEGREES = 0;
   //Swerve Drive Kinematics Constants
-  public static final SwerveDriveKinematics SWERVE_DRIVE_KINEMATICS = null;
+  public static final double ROBOT_BASE_WIDTH_METERS = 0;
+  public static final double ROBOT_BASE_LENGTH_METERS = 0;
+  public static final double X_FROM_CENTER = ROBOT_BASE_LENGTH_METERS / 2;
+  public static final double Y_FROM_CENTER = ROBOT_BASE_WIDTH_METERS / 2;
+//      Robot Orientation
+//               +x(front)
+//               _________
+//               |       |
+//     +y(left)  |       | -y(right)
+//               |       |
+//               |_______|
+//                -x(back)
+  public static final double FRONT_LEFT_X_LOCATION = X_FROM_CENTER;
+  public static final double FRONT_LEFT_Y_LOCATION = Y_FROM_CENTER;
 
+  public static final double FRONT_RIGHT_X_LOCATION = X_FROM_CENTER;
+  public static final double FRONT_RIGHT_Y_LOCATION = -Y_FROM_CENTER;
+
+  public static final double BACK_LEFT_X_LOCATION = -X_FROM_CENTER;
+  public static final double BACK_LEFT_Y_LOCATION = Y_FROM_CENTER;
+
+  public static final double BACK_RIGHT_X_LOCATION = -X_FROM_CENTER;
+  public static final double BACK_RIGHT_Y_LOCATION = -Y_FROM_CENTER;
+  public static final SwerveDriveKinematics SWERVE_DRIVE_KINEMATICS = new SwerveDriveKinematics(
+    new Translation2d(FRONT_LEFT_X_LOCATION, FRONT_LEFT_Y_LOCATION),
+    new Translation2d(FRONT_RIGHT_X_LOCATION, FRONT_RIGHT_Y_LOCATION),
+    new Translation2d(BACK_LEFT_X_LOCATION, BACK_LEFT_Y_LOCATION),
+    new Translation2d(BACK_RIGHT_X_LOCATION, BACK_RIGHT_Y_LOCATION));
+  //Deadband for joysticks
+    public static final double DEADBAND = 0;
 }
--- src/main/java/frc/robot/subsystems/Swerve/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/Swerve/SwerveSubsystem.java
@@ -2,7 +2,11 @@ package frc.robot.subsystems.Swerve;
 
 import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
@@ -40,6 +44,13 @@ public class SwerveSubsystem extends SubsystemBase{
             Constants.BACK_LEFT_ANGLE_MOTOR_ID, 
             Constants.BACK_LEFT_ANGLE_ENCODER_ID, 
             Constants.BACK_LEFT_ANGLE_OFFSET_DEGREES);
+        backRightModule = new SwerveModule(
+            Constants.BACK_RIGHT_MODULE_NAME, 
+            Constants.BACK_RIGHT_MODULE_NUMBER, 
+            Constants.BACK_RIGHT_DRIVE_MOTOR_ID, 
+            Constants.BACK_RIGHT_ANGLE_MOTOR_ID, 
+            Constants.BACK_RIGHT_ANGLE_ENCODER_ID, 
+            Constants.BACK_RIGHT_ANGLE_OFFSET_DEGREES);
 
         odometry = new SwerveDriveOdometry(Constants.SWERVE_DRIVE_KINEMATICS, gyro.getRotation2d(), getModulePositions());
         modules = new SwerveModule[]{frontLeftModule, frontRightModule, backLeftModule, backRightModule};
@@ -47,6 +58,51 @@ public class SwerveSubsystem extends SubsystemBase{
             driveTab.addNumber(module.getName() + " Current Angle:", () -> module.getAngle());
         }
         driveTab.addNumber("Gyro", () -> gyro.getRotation2d().getDegrees());
-        
+    }
+    @Override
+    public void periodic() {
+        odometry.update(gyro.getRotation2d(), getModulePositions());
+    }
+
+    public void drive(double xSpeed, double ySpeed, double zSpeed, boolean fieldOriented){
+        SwerveModuleState[] states;
+        if(fieldOriented){
+            states = Constants.SWERVE_DRIVE_KINEMATICS.toSwerveModuleStates(ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, zSpeed, gyro.getRotation2d()));
+        }else{
+            states = Constants.SWERVE_DRIVE_KINEMATICS.toSwerveModuleStates(new ChassisSpeeds(xSpeed, ySpeed, zSpeed));
+        }
+        for(SwerveModule module : modules){
+            System.out.println(module.getName() + " DriveSpeed: " + module.getDriveSpeed());
+            System.out.println(module.getName() + " Angle Speed: " + module.getAngleSpeed());
+            System.out.println(module.getName() + " Current Angle: " + module.getAngle());
+            System.out.println(module.getName() + " Wanted Angle: " + states[module.getModuleNumber()].angle.getDegrees());
+            System.out.println(module.getName() + " Optimized Angle: " + SwerveModuleState.optimize(states[module.getModuleNumber()], module.getState().angle));
+        }
+        setModuleStates(states);
+    }
+
+    public SwerveModuleState[] getModuleStates(){
+        return new SwerveModuleState[]{
+            frontLeftModule.getState(),
+            frontRightModule.getState(),
+            backLeftModule.getState(),
+            backRightModule.getState()
+        };
+    }
+
+    public SwerveModulePosition[] getModulePositions(){
+        return new SwerveModulePosition[]{
+            frontLeftModule.getPosition(),
+            frontRightModule.getPosition(),
+            backLeftModule.getPosition(),
+            backRightModule.getPosition()
+        };
+    }
+
+    public void setModuleStates(SwerveModuleState[] desiredStates){
+        SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, 1);
+        for(SwerveModule module: modules){
+            module.setState(desiredStates[module.getModuleNumber()]);
+        }
     }
 }
