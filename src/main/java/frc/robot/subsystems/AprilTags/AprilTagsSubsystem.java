4190a86d0d25d8cb4cc4ec7fd56e070a7e23a43b
Dakovan <d.camp.hovan@gmail.com>
2024-01-16 09:14:09 -0600

    Create AprilTagsSubsystem.java
    Not quite done.

---
+++ src/main/java/frc/robot/subsystems/AprilTags/AprilTagsSubsystem.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems.AprilTags;
+
+import org.photonvision.PhotonCamera;
+
+public class AprilTagsSubsystem {
+
+    PhotonCamera camera = new PhotonCamera("camera");
+
+    public void AprilTagsSubsystem() {
+        
+    }
+
+    public void CheckTarget() {
+        
+    }
+
+}
