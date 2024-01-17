eb520e0d90d6cea9ff884218723f9182b069c23a
41robocats98 <60160962+41robocats98@users.noreply.github.com>
2024-01-16 18:14:20 -0600

    save

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -4,10 +4,15 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase {
+
+  CANSparkMax motor;
+  
   /** Creates a new ExampleSubsystem. */
   public ExampleSubsystem() {}
 
