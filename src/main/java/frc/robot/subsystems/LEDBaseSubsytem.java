2555bef30fc95ca735dd4422b8affad3a5bf533d
41robocats98 <60160962+41robocats98@users.noreply.github.com>
2024-01-30 18:54:53 -0600

    Update LEDBaseSubsytem.java

--- src/main/java/frc/robot/subsystems/LEDBaseSubsytem.java
+++ src/main/java/frc/robot/subsystems/LEDBaseSubsytem.java
@@ -8,42 +8,66 @@ import com.revrobotics.CANSparkMax;
 import edu.wpi.first.hal.AddressableLEDJNI;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class LEDBaseSubsytem extends SubsystemBase {
-     
-private AddressableLED driveBaseLED =  new AddressableLED(Constants.AddressableLED);
 
-//length(num of stips)
-private AddressableLEDBuffer driveBaseLEDBuffer = new AddressableLEDBuffer(Constants.AddressableLEDBuffer);
+    private AddressableLED driveBaseLED = new AddressableLED(Constants.DRIVE_BASE_LED_ID);
 
-public AddressableLED.setData(driveBaseLEDBuffer);
+    // length(num of stips)
+    private AddressableLEDBuffer driveBaseLEDBuffer = new AddressableLEDBuffer(Constants.AddressableLEDBuffer);
 
-public LEDBaseSubsytem(){}
+    public LEDBaseSubsytem() {
+         driveBaseLED.setLength(driveBaseLEDBuffer.getLength());
+         driveBaseLED.setData(driveBaseLEDBuffer);
+         driveBaseLED.se
+         driveBaseLED.start();
+    }
 
-//setHSV() -> color saturation
+    // setHSV() -> color saturation
 
-public static void solidColors(){
+    // public void solidColors(){
     public void RGBColorRed() {
-    for (var i =0; i < driveBaseLEDBuffer.getLength(); i ++){
-        driveBaseLEDBuffer.setRGB(i, 255, 0, 0);}
+        for (var i = 0; i < driveBaseLEDBuffer.getLength(); i++) {
+            driveBaseLEDBuffer.setLED(i, Color.kRed);
+        }
     }
 
-    public void RGBColorBlue(){
-    for (var i =0; i < driveBaseLEDBuffer.getLength(); i ++){
-        driveBaseLEDBuffer.setRGB(i, 0, 0, 255);}}
-
-    public void RGBColorGreen(){
-    for (var i =0; i < driveBaseLEDBuffer.getLength(); i ++){
-        driveBaseLEDBuffer.setRGB(i, 0, 255, 0);}
+    public void RGBColorBlue() {
+        for (var i = 0; i < driveBaseLEDBuffer.getLength(); i++) {
+            driveBaseLEDBuffer.setRGB(i, 0, 0, 255);
+        }
     }
-}
+
+    public void RGBColorGreen() {
+        for (var i = 0; i < driveBaseLEDBuffer.getLength(); i++) {
+            driveBaseLEDBuffer.setRGB(i, 0, 255, 0);
+        }
+    }
+    // }
 
 public static void changingColor(){
 
+    private void rainbow
+    {
 
-    
+        for (var i = ; i < LedBuffer.getlength (); i ++){
+
+            final var hue = (m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getlength()))
+
+            m_ledBuffer.setHSV(i, hue, 255, 128);
+        }
+        
+        m_rainbowFirstPixelHue +=3;
+
+        m_rainbowFirstPixelHue %= 180;
+    }
+
 }
 
 }
+// No periodic 
+// change different sections of led seperatly 
+//
